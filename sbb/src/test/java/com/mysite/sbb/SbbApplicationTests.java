1c03133a0ab780edd128e5efe76d11f139236154
김교휘 <hwee000@naver.com>
2023-07-24 22:45:59 +0900

    2.6

--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -3,19 +3,24 @@ package com.mysite.sbb;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SbbApplicationTests {
 	@Autowired
 	private QuestionRepository questionRepository;
+	@Autowired
+	private AnswerRepository answerRepository;
 
 	@Test
+	@Transactional  //메서드가 종료될 때까지 DB 세션이 유지(DB와 연결이 안끊어짐)
 	void testJpa() {
 //		Question question1=new Question();
 //		question1.setSubject("sbb가 무엇인가요?");
@@ -48,6 +53,37 @@ class SbbApplicationTests {
 		//sbb% : sbb로 시작하는 문자열.  %sbb : sbb로 끝나는 문자열.  %sbb% : sbb를 포함하는 문자열
 		Question q4=qList.get(0);
 		assertEquals("sbb가 무엇인가요?", q4.getSubject());
+
+		//질문 수정하는 테스트 코드
+		Optional<Question> oq2=this.questionRepository.findById(1);
+		assertTrue(oq2.isPresent());
+		Question q5=oq2.get();
+		q5.setSubject("수정된 제목");
+		this.questionRepository.save(q5);
+
+		//데이터 삭제 테스트
+		assertEquals(2, this.questionRepository.count());
+		Optional<Question> oq3=this.questionRepository.findById(1);
+		assertTrue(oq3.isPresent());
+		Question q6=oq3.get();
+		this.questionRepository.delete(q6);
+		assertEquals(1, this.questionRepository.count());
+
+		//답변 데이터 생성 후 저장
+		Optional<Question> oq4=this.questionRepository.findById(2);
+		assertTrue(oq4.isPresent());
+		Question q7=oq4.get();
+		Answer answer=new Answer();
+		answer.setContent("네 자동으로 생성됩니다.");
+		answer.setQuestion(q7); //어떤 질문의 답변인지 알기 위하여 Question 객체 필요
+		answer.setCreateDate(LocalDateTime.now());
+		this.answerRepository.save(answer);
+
+		//답변 조회
+		Optional<Answer> oa=this.answerRepository.findById(1);
+		assertTrue(oa.isPresent());
+		Answer a=oa.get();
+		assertEquals(2, a.getQuestion().getId());
 	}
 
 }
