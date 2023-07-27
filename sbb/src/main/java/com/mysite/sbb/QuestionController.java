9a6ae198e245897d4b579189e021c9abcc03347b
김교휘 <hwee000@naver.com>
2023-07-27 16:49:52 +0900

    2.10

--- sbb/src/main/java/com/mysite/sbb/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/QuestionController.java
@@ -2,24 +2,36 @@ package com.mysite.sbb;
 
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+
 @Controller
 @RequiredArgsConstructor  //롬복 애너테이션, final 붙은 속성을 생성자에 자동 포함
+@RequestMapping("/question")  //클래스 단위의 매핑
 public class QuestionController {
-    private final QuestionRepository questionRepository;  //자동 주입(RequiredArgsConstructor에 의하여)
+    private final QuestionService questionService;  //자동 주입(RequiredArgsConstructor에 의하여)
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model){ //컨트롤러의 매개변수에 Model 객체 선언하면 자동으로 생성
-        List<Question> questionList=this.questionRepository.findAll();
+        List<Question> questionList=this.questionService.getList()  ;
         model.addAttribute("questionList", questionList);  //Model객체에 리스트 저장.
         //Model객체는 자바 클래스와 템플릿을 연결. 저장해두면 템플릿에서 사용 가능 
         return "question_list";
+    }
+
+    @GetMapping(value="/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id){
+        Question question=this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
---
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -0,0 +1,27 @@
+package com.mysite.sbb.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import com.mysite.sbb.DataNotFoundException;
+
+//서비스에서 return하는 것은 보통 DTO 객체이다.  리포지토리와 엔티티객체(모델)로 소통, 컨트롤러와 DTO객체로 소통
+//DTO는 비즈니스 모델에서 사용하는 객체. 즉 서비스는 엔티티-DTO 객체들을 양방향에 전달하는 역할
+@Service
+@RequiredArgsConstructor
+public class QuestionService {
+    private final QuestionRepository questionRepository;
+    public List<Question> getList(){
+        return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id){
+        Optional<Question> question=this.questionRepository.findById(id);
+        if(question.isPresent()) {
+            return question.get();
+        }
+        else throw new DataNotFoundException("question is not found");
+    }
+}
